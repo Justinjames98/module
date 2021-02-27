b55b59c839fc13a5f2c0747f82aa161e376989b0
Justinjames98 <justinkjames38@gmail.com>
2021-02-27 12:20:13 +0530

    calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+from calmodule import add,sub,mul,div
+
+a=int(input("enater a number"))
+b=int(input("enater a number"))
+
+addresult=add(a,b)
+print(addresult)
+
+subresult=sub(a,b)
+print(subresult)
+
+mulresult=mul(a,b)
+print(mulresult)
+
+divresult=div(a,b)
+print(divresult)
